0da108083aadc13d924ff36ffd79177d3ece25ad
stomaras <sptoma724@gmail.com>
2022-04-21 16:52:41 +0300

    Chess Tournament Construction Begins

---
+++ PeopleCertProjects/Labs/ChessTournament/ChessTournament/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessTournament tournament = new ChessTournament();
+            tournament.Start();
+
+            Player p1 = CreateRandomPlayer(); // new Player(1, "Pasparov", 0);
+
+        }
+
+        public static Player CreateRandomPlayer()
+        {
+            return (new Player(1,"AAA", 1000));
+        }
+    }
+}
---
+++ PeopleCertProjects/Labs/ChessTournament/ChessTournament/domain/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTournament
+{
+    class Player
+    {
+        // Id, Name, Ranking
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _ranking;
+
+        public int Ranking
+        {
+            get { return _ranking; }
+            set { _ranking = value; }
+        }
+
+
+    }
+}
