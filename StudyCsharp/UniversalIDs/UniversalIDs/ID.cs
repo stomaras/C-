7be1f70f8a8d9bdb7ca4880579bb08e5296683f8
stomaras <sptoma724@gmail.com>
2022-05-04 12:11:05 +0300

    Universal IDs

---
+++ StudyCsharp/UniversalIDs/UniversalIDs/ID.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalIDs
+{
+    class ID
+    {
+        public string AMKA;
+        public string AFM;
+        public string ADT;
+    }
+}
---
+++ StudyCsharp/UniversalIDs/UniversalIDs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalIDs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonId<string, ID>> keyValuePairs = new List<PersonId<string, ID>>();
+            keyValuePairs.Add(new PersonId<string, ID>("PASPARAKIS GEORGIOS", new ID()));
+            Console.WriteLine(keyValuePairs[0]);
+        }
+
+        struct PersonId<Name, ID>
+        {
+            public PersonId(Name name, ID ids)
+            {
+
+            }
+        }
+    }
+}
