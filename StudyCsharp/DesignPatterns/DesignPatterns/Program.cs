7fba1bf0117530b06bddfea52f5dd81e68c4e187
stomaras <sptoma724@gmail.com>
2022-05-18 11:07:11 +0300

    Design Patterns Beginsd

---
+++ StudyCsharp/DesignPatterns/DesignPatterns/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Design Patterns are evolved as reusable solutions to the problems that we encounter every day of programming 
+            // make apps maintainable, they are generally targeted at solving the problems of object generation and integration
+            // in other words acts as templates which applied to the real world programming problems 
+            // The four authors of the book "Elements of reusable Object-Oriented-Software" are reffered as Gang of Four.
+            // The book is divided into two parts with first part explaining about the pros and cons of Object Oriented programming and second part 
+            // explaining the evolution of 23 classic software design patterns.
+            // There are 3 types of Design Patterns
+            // 1) Creational 
+            // This type deals with the object creation and initialization. This pattern gives the program more flexibility
+            // in deciding which objects need to be created for a given case.
+            // Eg: Singleton, Factory, Abstract Factory ...etc
+
+            // 2) Structural
+            // This type deals with class and object composition
+            // This pattern focuses on decoupling interface and implementation of classes and its objects.
+            // Eg: Adapter, Bridge .. ETC
+
+            // 3) Behavioural
+            // This Type deals with the communication between Classes and objects
+            // Eg: Chain of Responsibility, Command, Interpreter ... etc
+        }
+    }
+}
---
+++ StudyCsharp/DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /*
+     * What is Singleton Design Pattern
+     * Singleton as Creational Pattern
+     * Implementation Guidelines
+     * How do we Implement a Singleton class
+     * 
+     * Singleton Pattern 
+     * Singleton Pattern belongs to Creational Type Pattern.
+     * 
+     * This pattern is used when we need to ensure that only one object of a particular class need to be created. 
+     * All further References to the objects are reffered to the same underlying instance created
+     * Advantages of Singleton
+     * >> Singleton controls concurrent access to the resource.
+     * >> It ensures there is only one object available across the application in a controlled state.
+     * 
+     * >> Implementation Guidelines
+     * >> Ensure that only one instance of the class exists
+     * >> Provide global access to that instance by 
+     * >>>> Declaring all constructors of the class to be private
+     * >>>> Providing static method that returns a reference to the instance
+     * >>>> The instance is stored as a private static variable.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("thgis is first message");
+            fromEmployee.PrintDetails("something");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        
+        }
+    }
+}
