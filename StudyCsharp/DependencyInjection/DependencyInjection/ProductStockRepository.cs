40a97de3e490869fef71f8c7749e2d8723a5aa31
stomaras <sptoma724@gmail.com>
2022-05-22 16:41:17 +0300

    Dependecy Injection Console App  Start

---
+++ StudyCsharp/DependencyInjection/DependencyInjection/ProductStockRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class ProductStockRepository
+    {
+        public static Dictionary<Product, int> _productStockDatabase = Setup();
+
+        private static Dictionary<Product, int> Setup()
+        {
+            var productStockDatabase = new Dictionary<Product, int>();
+            productStockDatabase.Add(Product.Keyboard, 1);
+            productStockDatabase.Add(Product.Mic, 1);
+            productStockDatabase.Add(Product.Mouse, 1);
+            productStockDatabase.Add(Product.Speaker, 1);
+            return productStockDatabase;
+        }
+
+        // We want to check if a product is in stock 
+        public bool IsInStock(Product product)
+        {
+            Console.WriteLine("Call Get On Database");
+            return _productStockDatabase[product] > 0;
+        }
+    }
+}
---
+++ StudyCsharp/DependencyInjection/DependencyInjection/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Let's send the product to order manager 
+            var product = string.Empty;
+            var orderManager = new OrderManager();
+            while (product != "exit")
+            {
+                Console.WriteLine("Enter a Product:",
+                    "Keyboard = 0",
+                    "Mouse = 1",
+                    "Mic = 2",
+                    "Speaker = 3",
+                    "");
+                product = Console.ReadLine();
+                if (Enum.TryParse(product, out Product productEnum))
+                {
+                    Console.WriteLine("Please enter a valid payment method: xxxxxxxxxxxxxxxx;MMYY");
+                    var paymentMethod = Console.ReadLine();
+                    if (string.IsNullOrEmpty(paymentMethod) || !paymentMethod.Split(";").Length == 2)
+                    {
+                        throw new Exception("Invalid Payment Method");
+                    }
+                    orderManager.Submit(product)
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Product");
+                }
+            }
+        }
+    }
+}
