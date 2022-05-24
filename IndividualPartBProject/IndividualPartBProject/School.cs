f475cfc95cca53471fbf9bdc3b41ff6e19a5a81b
stomaras <sptoma724@gmail.com>
2022-05-24 11:25:08 +0300

    Individual Part B

---
+++ IndividualPartBProject/IndividualPartBProject/School.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndividualPartBProject.Models;
+using IndividualPartBProject.Services;
+
+namespace IndividualPartBProject
+{
+    class School
+    {
+        public int NumOfAssigments { get; set; }
+        public int NumOfCourses { get; set; }
+        public int NumOfStudents { get; set; }
+        public int NumOfTrainers { get; set; }
+        public School() { }
+
+        public School(int numOfAssigments, int numOfCourses, int numOfStudents, int numOfTrainers)
+        {
+
+        }
+        public void Start()
+        {
+            
+        }
+    }
+}
