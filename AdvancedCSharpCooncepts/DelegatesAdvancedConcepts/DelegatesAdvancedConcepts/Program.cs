f74e8fd07ef57ea57ccdcbb24919318f70666df7
stomaras <sptoma724@gmail.com>
2022-05-28 18:53:55 +0300

    Advanced Delegates Examples 2/3 done

---
+++ AdvancedCSharpCooncepts/DelegatesAdvancedConcepts/DelegatesAdvancedConcepts/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoooLib;
+
+namespace DelegatesAdvancedConcepts
+{
+    /*
+     * When you pass in information to a method delegate is same kind of thing but you pass a method in
+     * Delegates are essentially methods you pass around and you can execute in other locations 
+     * 
+     * 
+     */
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The Total for the cart is {cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount, AlertUser):C2}");
+        }
+
+        private static void SubTotalAlert(decimal subTotal)
+        {
+            Console.WriteLine($"The subtotal is {subTotal:C2}");
+        }
+
+        private static void AlertUser(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static decimal CalculateLeveledDiscount(List<ProductModel> items, decimal subTotal)
+        {
+            if (subTotal > 100)
+            {
+                return subTotal * 0.80M;
+            }
+            else if (subTotal > 50)
+            {
+                return subTotal * 0.85M;
+            }
+            else if (subTotal > 10)
+            {
+                return subTotal * 0.90M;
+            }
+            else
+            {
+                return subTotal;
+            }
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.64M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.51M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.84M });
+        }
+    }
+}
---
+++ AdvancedCSharpCooncepts/DelegatesAdvancedConcepts/DemoooLib/ShoppingCartModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoooLib
+{
+    /*
+     * This delegate as an interface
+     * Demo Library maybe used by 10 or 12 different projects , so if you have to change sometging
+     * shit happens
+     * What if you decide to make discount based on items in shopping cart and not the dollars?????
+     * open closed principle says that we shouldn't be making changes this  class if we can help it
+     * Action delegate take a parameter and returns nothing.
+     */
+    public delegate void MentionDiscount(decimal subtotal);
+    public class ShoppingCartModel
+    {
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+
+        public decimal GenerateTotal(MentionDiscount mentionDiscount, Func<List<ProductModel>,decimal,decimal> calculateDiscounteedTotal,
+            Action<string> tellUserWeAreDiscounting)
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            mentionDiscount.Invoke(subTotal);
+
+            tellUserWeAreDiscounting("We are applying your discount.");
+
+            return calculateDiscounteedTotal(Items, subTotal);
+            //if (subTotal > 100)
+            //{
+            //    return subTotal * 0.80M;
+            //}
+            //else if (subTotal > 50)
+            //{
+            //    return subTotal * 0.85M;
+            //}
+            //else if (subTotal > 10)
+            //{
+            //    return subTotal * 0.90M;
+            //}
+            //else
+            //{
+            //    return subTotal;
+            //}
+        }
+    }
+}
