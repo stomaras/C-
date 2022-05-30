b2bc02c6ebf7bdbbacf81defc821581206defd0f
stomaras <sptoma724@gmail.com>
2022-05-30 22:05:31 +0300

    Boring

---
+++ Boring/Boring/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Boring
+{
+    // ORM = Object Relation Mappers
+    // EF6 -----> Database First : take data and convert to c# app
+    //     -----> Code First : from a csharp application i create database to sq server.
+    //     -----> UML TO OBJECTS
+    // 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            using (boringDataEntities db = new boringDataEntities())
+            {
+                
+
+                var students = db.Student.Where(x => x.salary > 1000).OrderBy(y => y.name);
+
+                Console.WriteLine(students);
+                Console.WriteLine("-----------------------READ----------------------------------------------");
+
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"{student.student_id,-15}{student.name,-10}{student.salary,15}");
+                }
+                //Console.WriteLine("-----------------------CREATE-------------------------------------");
+
+                //Student s = new Student() { name = "Tom", salary = 38000 }; // Synthetc data
+
+                //db.Entry(s).State = EntityState.Added; // is going to added in sql server
+
+                //db.SaveChanges();
+
+                //Console.WriteLine("----------------------------DELETE---------------------------------");
+                //Console.WriteLine("Choose student to delete");
+                //int id = Convert.ToInt32(Console.ReadLine());
+                //var student2 = db.Student.Find(id); // null or student
+
+                //if (student2 != null)
+                //{
+                //    db.Entry(student2).State = EntityState.Deleted;
+                //    db.SaveChanges();
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Student does not exists");
+                //}
+
+                Console.WriteLine("------------------------EDIT------------------------------------------");
+                Console.WriteLine("Choose Student For Edit:\n");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                var studentForEdit = db.Student.Find(id);// null or student
+                if (studentForEdit == null)
+                {
+                    Console.WriteLine("Student does not exist!");
+                    return; // in main method --> go to .exe --> 404
+                }
+
+                Console.WriteLine($"{studentForEdit.student_id,-15}{studentForEdit.name,-10}{studentForEdit.salary,15}");
+
+                Console.WriteLine("Give new Name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Gve you salary");
+                int salary = Convert.ToInt32(Console.ReadLine());
+
+                studentForEdit.name = name;
+                studentForEdit.salary = salary;
+
+                db.Entry(studentForEdit).State = EntityState.Modified;
+                db.SaveChanges();
+
+
+            }
+
+        }
+    }
+}
---
+++ Boring/Boring/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Boring")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Boring")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7e06cc4-eec4-4e34-a36b-1f9f0485ab81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
