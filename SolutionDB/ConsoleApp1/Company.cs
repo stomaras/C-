a0160d0b2c624f1df6438f281775be32cf29f11c
stomaras <sptoma724@gmail.com>
2022-06-25 13:00:17 +0300

    Holl 7

---
+++ SolutionDB/ConsoleApp1/Company.cs
@@ -0,0 +1,37 @@
+﻿using MyDatabase;
+using Repositories.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Company
+    {
+
+        public void Start()
+        {
+            //MyDBContext dBContext = new MyDBContext();
+            //UnitOfWork CompanyUnit = new UnitOfWork(dBContext);
+
+            //var employees = CompanyUnit.Employees.EmployeesWithProjects();
+
+            //foreach (var employee in employees)
+            //{
+
+            //    if (employee.Project == null)
+            //    {
+            //        string projectName = "No Project Yet";
+            //        Console.WriteLine($"Employee with first name {employee.FirstName}, with last name {employee.LastName}, with project name {projectName}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Employee with first name {employee.FirstName}, with last name {employee.LastName}, with project name {employee.Project.ProjectName}");
+
+            //    }
+            //}
+        }
+    }
+}
--- SolutionDB/ConsoleApp1/Program.cs
+++ SolutionDB/ConsoleApp1/Program.cs
@@ -13,25 +13,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            MyDBContext dBContext = new MyDBContext();
-            UnitOfWork CompanyUnit = new UnitOfWork(dBContext);
-
-            var employees = CompanyUnit.Employees.EmployeesWithProjects();
-
-            foreach (var employee in employees)
-            {
-                
-                if (employee.Project == null)
-                {
-                    string projectName = "No Project Yet";
-                    Console.WriteLine($"Employee with first name {employee.FirstName}, with last name {employee.LastName}, with project name {projectName}");
-                }
-                else
-                {
-                    Console.WriteLine($"Employee with first name {employee.FirstName}, with last name {employee.LastName}, with project name {employee.Project.ProjectName}");
-
-                }
-            }
+            Company company = new Company();
+            company.Start();
+            
             
         }
     }
