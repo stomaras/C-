76a506b8995739680a7842f74a4dd2f71a4d4331
stomaras <sptoma724@gmail.com>
2022-07-05 00:29:23 +0300

    hol

---
+++ SolutionDB/HollywoodApi (1)/Hollywood/FinalApp/Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalApp.Areas.Admin.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Admin/Movie
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SolutionDB/HollywoodApi (1)/Hollywood/RepositoryServices/Core/Repositories/IMovieRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryServices.Core.Repositories
+{
+    public interface IMovieRepository : IGenericRepository<Movie>
+    {
+        IEnumerable<Movie> GetMoviesOrderByAscending();
+        IEnumerable<Movie> GetBestMovies();
+        IEnumerable<Movie> GetTopMoviesByGenre(string genre,int count);
+        IEnumerable<Movie> GetLongestMovies();
+        IEnumerable<Movie> GetOldestMovies();
+        IEnumerable<Movie> GetRelatedMovies(int? id);
+        
+    }
+}
+
