8df85670d09183562a230c0439e463d6a5248ef9
stomaras <sptoma724@gmail.com>
2022-07-05 13:34:05 +0300

    holl

---
+++ HollywoodApi (1)/Hollywood/FinalApp/Areas/Customer/ViewModels/Movie_Details_ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalApp.Areas.Customer.ViewModels
+{
+    public class Movie_Details_ViewModel
+    {
+        public Movie Movie { get; set; }
+        public IEnumerable<Movie> RelatedMovies  { get; set; }
+    }
+}
