582e13ba581c3bf4895d882336c75541d636cf18
stomaras <sptoma724@gmail.com>
2022-07-12 00:28:26 +0300

    dd

---
+++ JSProjects-main/APIPRODUCT/Repositories/Persistance/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+using MyDatabase;
+using Repositories.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using MyDatabase;
+
+namespace Repositories.Persistance.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public IEnumerable<Product> GetAllProductsWithShop()
+        {
+
+            var productsWithShop = db.Products.Include(x => x.Shop);
+            return productsWithShop;
+            
+            
+            
+        }
+    }
+}
---
+++ JSProjects-main/Section 7/GuessNumber/starter/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+*/
+
+/*
+Define Secret Number Here
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // state variable
+let score = 20; // state variable
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('❌ No number');
+  } else if (guess == secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess i wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too High!' : '📉 Too Low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('❗ You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  let score = 20;
+  let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+});
