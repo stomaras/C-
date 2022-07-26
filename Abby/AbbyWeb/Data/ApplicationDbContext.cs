b6ca59aecf8267e22e985bc5f8207e9348c70a43
stomaras <sptoma724@gmail.com>
2022-07-26 16:59:10 +0300

    Abby

---
+++ Abby/AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AbbyWeb.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbbyWeb.Data
+{
+    // We need to configure this DbContext to use our connection string . That way, it will know how it can connect to the database
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ Abby/AbbyWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+/*
+ * Razor Pages Need A Root Folder, we can find this insed Page folder
+ * Index.cshtml is a default document
+ * 
+ * 
+ */
+namespace AbbyWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        // In razor pages we call controllers as Handlers
+        public void OnGet()
+        {
+
+        }
+    }
+}
